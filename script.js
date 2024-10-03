c79e92a254e55da828a868256f4d8344235e7ccd
Nirmalya M. Thakur <nirmalyamallickthakur@gmail.com>
2024-10-03 14:10:35 +0530

    update home, cv, projects pages

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-document.getElementById('themeToggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("themeToggle")
+    const icon = themeToggle.querySelector("i")
+    const navbar = document.getElementsByClassName("navbar")[0]
+
+    // Check for saved theme preference or default to light theme
+    const savedTheme = localStorage.getItem("theme") || "light"
+    document.body.classList.toggle("dark-mode", savedTheme === "dark")
+    setTheme(savedTheme == "dark")
+
+    themeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode")
+        const isDarkMode = document.body.classList.contains("dark-mode")
+        localStorage.setItem("theme", isDarkMode ? "dark" : "light")
+        setTheme(isDarkMode)
+    })
+
+    function setTheme(isDarkMode) {
+        document.body.classList.toggle("dark-mode", isDarkMode)
+        // navbar.classList.toggle('navbar-light', !isDarkMode)
+        navbar.classList.toggle("navbar-dark", isDarkMode)
+        updateIcon(isDarkMode)
+    }
+
+    function updateIcon(isDarkMode) {
+        if (isDarkMode) {
+            icon.classList.remove("fa-sun")
+            icon.classList.add("fa-moon")
+        } else {
+            icon.classList.remove("fa-moon")
+            icon.classList.add("fa-sun")
+        }
+    }
+})
